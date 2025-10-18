658d71d740983293d452762fa9f514f2519fc8c9
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2025-10-18 11:58:23 -0400

    Create main.cpp

---
+++ .test/main.cpp
@@ -0,0 +1,49 @@
+#include "Card.hpp"
+#include "Deck.hpp"
+#include "Hand.hpp"
+
+#include <iostream>
+
+int main() {
+    ////////////// Test Card ////////////////
+    Card c(1, 2);
+    std::cout << c.toString() << std::endl;
+    
+    ////////////// Test Deck ////////////////
+    int seed;
+    std::cout << "Enter seed: ";
+    std::cin >> seed; 
+    
+    Deck myDeck(seed);
+    while (myDeck.moreCards()) {    
+        std::cout << myDeck.dealCard().toString() << std::endl;
+    }
+    
+    myDeck.shuffle();   
+    std::cout << "******************AFTER SHUFFLING******************" << std::endl;
+    while (myDeck.moreCards()){
+        std::cout << myDeck.dealCard().toString() << std::endl;
+    }
+    
+    ////////////// Test Hand ////////////////
+    for (int i = 0; i < 5; i++) {
+        myDeck.shuffle();
+        while (myDeck.moreCards()){
+            std::cout << "------------------\n";
+            Hand h(myDeck);
+            h.printHand();
+            if (h.threeOfAKind()) {
+                std::cout << "Hand contains three of a kind" << std::endl;
+            } 
+            else if (h.twoPair()) {
+                std::cout << "Hand contains two pairs" << std::endl;
+            } 
+            else if (h.pair()) {
+                std::cout << "Hand contains a pair" << std::endl;
+            }
+            std::cout << "------------------\n";
+        }
+    }
+
+    return 0;
+}
