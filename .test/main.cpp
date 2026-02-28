7faa0ee7906dbfedf315cfa7f43f8d118860387e
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2026-02-28 12:00:38 -0500

    Update main.cpp

--- .test/main.cpp
+++ .test/main.cpp
@@ -6,7 +6,7 @@
 
 int main() {
     ////////////// Test Card ////////////////
-    Card c(1, 2);
+    Card c(10, 2);
     std::cout << c.toString() << std::endl;
     
     ////////////// Test Deck ////////////////
@@ -15,35 +15,53 @@ int main() {
     std::cin >> seed; 
     
     Deck myDeck(seed);
+    std::cout << "\n******************BEFORE SHUFFLING******************" << std::endl;
     while (myDeck.moreCards()) {    
-        std::cout << myDeck.dealCard().toString() << std::endl;
+        std::cout << myDeck.dealCard().toString() << ' ';
     }
     
     myDeck.shuffle();   
-    std::cout << "******************AFTER SHUFFLING******************" << std::endl;
+    std::cout << "\n******************AFTER SHUFFLING******************" << std::endl;
     while (myDeck.moreCards()){
-        std::cout << myDeck.dealCard().toString() << std::endl;
+        std::cout << myDeck.dealCard().toString() << ' ';
     }
-    
+    std::cout << '\n';
+
     ////////////// Test Hand ////////////////
-    for (int i = 0; i < 5; i++) {
-        myDeck.shuffle();
-        while (myDeck.moreCards()){
-            std::cout << "------------------\n";
-            Hand h(myDeck);
-            h.printHand();
-            if (h.threeOfAKind()) {
-                std::cout << "Hand contains three of a kind" << std::endl;
-            } 
-            else if (h.twoPair()) {
-                std::cout << "Hand contains two pairs" << std::endl;
-            } 
-            else if (h.pair()) {
-                std::cout << "Hand contains a pair" << std::endl;
-            }
-            std::cout << "------------------\n";
+    Hand h;
+    myDeck.shuffle();
+
+    bool pair = false;
+    bool two_pairs = false;
+    bool three = false;
+
+    while (true) {
+        h.takeCards(myDeck);
+        if (!h.isFullHand()) {
+            myDeck.shuffle();
+            h.takeCards(myDeck);
+        }            
+        std::cout << "------------------\n";
+        h.print();
+        if (h.hasThreeOfAKind()) {
+            std::cout << "Hand contains three of a kind" << std::endl;
+            three = true;
+        } 
+        else if (h.hasTwoPairs()) {
+            std::cout << "Hand contains two pairs" << std::endl;
+            two_pairs = true;
+        } 
+        else if (h.hasPair()) {
+            std::cout << "Hand contains a pair" << std::endl;
+            pair = true;
+        }
+        std::cout << "------------------\n";
+
+        if (pair && two_pairs && three) {
+            break;
         }
     }
+
 
     return 0;
 }
